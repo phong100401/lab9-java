35ea251265f57fcf876a82e1cb54ba66b1111953
phong100401 <65002403+phong100401@users.noreply.github.com>
2020-10-25 12:00:29 +0700

    lab9-java
    lab9-java

---
+++ L9/TestC.java
@@ -0,0 +1,11 @@
+package L9;
+
+public class TestC {
+    public static void main(String[] args) {
+        Circle c1 = new Cylinder(1.1, 2.2);
+        System.out.println(c1.getRadius());
+
+        System.out.println(c1.toString());
+        System.out.println(c1.getArea());
+    }
+}
